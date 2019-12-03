86e4cb1f333e6af76aecf04e391df94d36ee768a
Kevin Frei <kevinfrei@hotmail.com>
2019-12-02 17:26:02 -0800

    Trivial little logger wrapper

---
+++ logger.js
@@ -0,0 +1,28 @@
+// @flow
+const disabled/*: Set<mixed>*/ = new Set();
+const enabled/*: Set<mixed>*/ = new Set();
+
+let defaultToShow = true;
+
+const log = (id/*: mixed*/, ...args/*: Array<mixed>*/) => {
+  if (
+    (defaultToShow && !disabled.has(id)) ||
+    (!defaultToShow && enabled.has(id))
+  ) {
+    console.log(...args);
+  }
+};
+
+log.disable = (id/*: mixed*/) => {
+  disabled.add(id);
+};
+
+log.enable = (id/*: mixed*/) => {
+  disabled.delete(id);
+};
+
+log.defaultToOff = () => defaultToShow = false;
+log.defaultToOn = () => defaultToShow = true;
+
+module.exports = log;
+
